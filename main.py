0cc02754419232353149e860e06b37e9c77c8af2
Vlad <dr.melix@gmail.com>
2024-09-26 18:56:29 +0300

    Хендлеры и главный файл

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def main():
-    pass
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher
+from aiogram.client.session.aiohttp import AiohttpSession
+from aiogram.enums import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
+from bot.config import BOT_TOKEN
+from bot.handlers import router
+
+
+async def main():
+    """Запуск бота и диспетчера"""
+    logging.basicConfig(level=logging.INFO)
+    session = AiohttpSession()
+    bot = Bot(token=BOT_TOKEN, session=session, parse_mode=ParseMode.HTML)
+    dp = Dispatcher(storage=MemoryStorage())
+    dp.include_router(router)
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
